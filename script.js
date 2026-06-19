cecb20ed1ec29b3be8f4f9f2aa9ffbe2d791f159
Hagar Saber <hagarsaber20077@gmail.com>
2026-06-19 07:49:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,118 @@
+// const menuBtn = document.getElementById("menuBtn");
+// const navLinks = document.getElementById("navLinks");
+// menuBtn.addEventListener("click",()=> {
+//   navLinks.classList.toggle("active")
+// });
+
+// function openOrder(name,price) {
+//   document.getElementById("productName").value = name;
+//   document.getElementById("price").value = price;
+//   document.getElementById("orderModal").style.display="block"
+//   // const customerName = prompt("Please enter your name.");
+  
+//   // if (!customerName || customerName.trim() === "") {
+//   //   alert("❌ You must enter your name.!");
+//   //   return;
+//   // }
+
+//   // try {
+//   //   const response = await fetch('http://localhost:5000/api/orders', {
+//   //     method: 'POST',
+//   //     headers: { 
+//   //       'Content-Type': 'application/json' 
+//   //     },
+//   //     body: JSON.stringify({
+//   //       customerName: customerName.trim(),
+//   //       productId: productId,
+//   //       quantity: 1
+//   //     })
+//   //   });
+
+//   //   const data = await response.json();
+
+//   //   if (data.success) {
+//   //     alert(`✅ Your order has been placed successfully!\n\n📦 Product: ${productName}\n💰 Price: ${price} EGP\n👤 Customer: ${customerName}\n\nThank you for choosing Sweet Delight. We hope you enjoy your dessert! 🍰`);
+//   //   } else {
+//   //     alert(`❌ Error: ${data.message}`);
+//   //   }
+//   // } catch (error) {
+//   //   alert(`❌ Connection error: Make sure the server is running on port 5000`);
+//   //   console.error('Error:', error);
+//   // }
+// }
+
+// async function loadProducts() {
+//   try {
+//     const response = await fetch('http://localhost:5000/api/products');
+//     const data = await response.json();
+    
+//     if (data.success) {
+//     console.log('✅ Products loaded successfully:', data.data);    }
+//   } catch (error) {
+//     console.error('Error loading products:', error);
+//   }
+// }
+// function toggleMore() {
+//   var moreText = document.getElementById("more");
+//   var btn = document.getElementById("btn");
+//   var dots = document.getElementById("dots")
+
+//    if (moreText.style.display === "none") {
+//     moreText.style.display = "inline";
+//     dots.style.display ="none"
+//     btn.innerHTML = "Read Less";
+//   } else {
+//     moreText.style.display = "none";
+//     dots.style.display = "inline"
+//     btn.innerHTML = "Read More";
+//   }
+// }
+// function more_img() {
+//   var imgs = document.getElementById("more-img");
+//   var btn = document.getElementById("btn-img");
+
+//   imgs.classList.toggle("show");
+
+//   if (imgs.classList.contains("show")) {
+//     btn.innerHTML = "View Less";
+//   } else {
+//     btn.innerHTML = "View More";
+//   }
+// }
+
+
+// let price = document.getElementById("price");
+// let quantity = document.getElementById("quantity");
+// let total = document.getElementById("total");
+// quantity.addEventListener("input",() => {total.textContent = price.value * quantity.value})
+
+
+
+// document.getElementById("orderForm").addEventListener("submit", async function(e) {
+//   e.preventDefault();
+
+//   const order = {
+//     customerName: document.getElementById("customerName").value,
+//     phone: document.getElementById("phone").value,
+//     productName: document.getElementById("productName").value,
+//     price: Number(document.getElementById("price").value),
+//     quantity: Number(document.getElementById("quantity").value),
+//     total: Number(document.getElementById("price").value) *
+//            Number(document.getElementById("quantity").value)
+//   };
+
+//   // هنا الربط بالباك إند
+//   await fetch("http://localhost:5000/api/orders", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(order)
+//   });
+
+//   alert("Order Placed Successfully 🚀");
+// });
+
 const menuBtn = document.getElementById("menuBtn");
 const navLinks = document.getElementById("navLinks");
 
@@ -124,6 +239,4 @@ window.onclick = function(e){
     if(e.target === modal){
         closeOrder();
     }
-}
-
 }
